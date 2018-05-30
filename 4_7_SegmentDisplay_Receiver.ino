cef24058af41aefd0948e7c293af042e8ce76c07
N0767938 <35261487+N0767938@users.noreply.github.com>
2018-05-30 18:09:37 +0100

    Add files via upload
    Attach are the file for my code for the 2nd course work XBEE wireless sensor network

---
+++ 4_7_SegmentDisplay_Receiver.ino
@@ -0,0 +1,232 @@
+
+/*
+  Showing number 0-9 on a Common Anode 7-segment LED display
+  Displays the numbers 0-9 on the display, with one second in between.
+    A
+   ---
+F |   | B
+  | G |
+   ---
+E |   | C
+  |   |
+   ---
+    D
+  
+ */
+ 
+// Pin 2-8 is connected to the 7 segments of the display.
+
+int pinA = 2;
+int pinB = 3;
+int pinC = 4;
+int pinD = 5;
+int pinE = 6;
+int pinF = 7;
+int pinG = 8;
+int D1 = 9;
+int D2 = 10;
+int D3 = 11;
+int D4 = 12;
+
+                  // the setup routine runs once when you press reset:
+void setup() 
+{                
+               // initialize the digital pins as outputs.
+  pinMode(pinA, OUTPUT);     
+  pinMode(pinB, OUTPUT);     
+  pinMode(pinC, OUTPUT);     
+  pinMode(pinD, OUTPUT);     
+  pinMode(pinE, OUTPUT);     
+  pinMode(pinF, OUTPUT);     
+  pinMode(pinG, OUTPUT);   
+  pinMode(D1, OUTPUT);  
+  pinMode(D2, OUTPUT);  
+  pinMode(D3, OUTPUT);  
+  pinMode(D4, OUTPUT);  
+}
+int counter = 0;
+                  // the loop routine runs over and over again forever:
+void loop() 
+  {
+
+  if(counter == 0)
+  {
+ digitalWrite(D1, HIGH);
+  digitalWrite(D2, HIGH);
+  digitalWrite(D3, HIGH);
+  digitalWrite(D4, LOW);
+  
+  //for 7 segment to display 0 only pin G is LOW
+  digitalWrite(pinA, HIGH);   
+  digitalWrite(pinB, HIGH);   
+  digitalWrite(pinC, HIGH);   
+  digitalWrite(pinD, HIGH);   
+  digitalWrite(pinE, HIGH);   
+  digitalWrite(pinF, HIGH);   
+  digitalWrite(pinG, LOW);   
+  delay(1000);               //The waiting time for 1 second
+
+  }
+
+ else if(counter ==1)
+  {
+ digitalWrite(D1, HIGH);
+  digitalWrite(D2, HIGH);
+  digitalWrite(D3, HIGH);
+  digitalWrite(D4, LOW);
+  //for 7 segment to display 1 only pinB &pinC are LOW
+  digitalWrite(pinA, HIGH);   
+  digitalWrite(pinB, LOW);   
+  digitalWrite(pinC, LOW);   
+  digitalWrite(pinD, HIGH);   
+  digitalWrite(pinE, HIGH);   
+  digitalWrite(pinF, HIGH);   
+  digitalWrite(pinG, HIGH);   
+  delay(1000);               // /The waiting time for 1 second
+
+  }
+ else if(counter ==2)
+  {
+ digitalWrite(D1, HIGH);
+  digitalWrite(D2, HIGH);
+  digitalWrite(D3, HIGH);
+  digitalWrite(D4, LOW);
+                 //for 7 segment to display 2 only pinC & pinF are HIGH
+                 
+  digitalWrite(pinA, LOW);   
+  digitalWrite(pinB, LOW);   
+  digitalWrite(pinC, HIGH);   
+  digitalWrite(pinD, LOW);   
+  digitalWrite(pinE, LOW);   
+  digitalWrite(pinF, HIGH);   
+  digitalWrite(pinG, LOW);   
+  delay(1000);               ///The waiting time for 1 second
+  }
+
+ else if(counter ==3)
+  {
+  digitalWrite(D1, HIGH);
+  digitalWrite(D2, HIGH);
+  digitalWrite(D3, HIGH);
+  digitalWrite(D4, LOW);
+                //for 7 segment to display 3 only pinE and pinF is HIGH
+  digitalWrite(pinA, LOW);   
+  digitalWrite(pinB, LOW);   
+  digitalWrite(pinC, LOW);   
+  digitalWrite(pinD, LOW);   
+  digitalWrite(pinE, HIGH);   
+  digitalWrite(pinF, HIGH);   
+  digitalWrite(pinG, LOW);   
+  delay(1000);               ///The waiting time for 1 second
+
+  }
+else if(counter ==4)
+  {
+  digitalWrite(D1, HIGH);
+  digitalWrite(D2, HIGH);
+  digitalWrite(D3, HIGH);
+  digitalWrite(D4, LOW);
+                 //for 7 segment to display 4 only pina, pinD & pinE are HIGH
+  digitalWrite(pinA, HIGH);   
+  digitalWrite(pinB, LOW);   
+  digitalWrite(pinC, LOW);   
+  digitalWrite(pinD, HIGH);   
+  digitalWrite(pinE, HIGH);   
+  digitalWrite(pinF, LOW);   
+  digitalWrite(pinG, LOW);   
+  delay(1000);               // /The waiting time for 1 second
+
+  }
+
+  else if(counter ==5)
+  {
+  digitalWrite(D1, HIGH);
+  digitalWrite(D2, HIGH);
+  digitalWrite(D3, HIGH);
+  digitalWrite(D4, LOW);
+                  //for 7 segment to display 5 only pinB and pinE are HIGH
+  digitalWrite(pinA, LOW);   
+  digitalWrite(pinB, HIGH);   
+  digitalWrite(pinC, LOW);   
+  digitalWrite(pinD, LOW);   
+  digitalWrite(pinE, HIGH);   
+  digitalWrite(pinF, LOW);   
+  digitalWrite(pinG, LOW);   
+  delay(1000);               ///The waiting time for 1 secondd
+
+  }
+  else if(counter ==6)
+  {
+ digitalWrite(D1, HIGH);
+  digitalWrite(D2, HIGH);
+  digitalWrite(D3, HIGH);
+  digitalWrite(D4, LOW);
+               //for 7 segment to display 6 only pinB is HIGH
+  digitalWrite(pinA, LOW);   
+  digitalWrite(pinB, HIGH);   
+  digitalWrite(pinC, LOW);   
+  digitalWrite(pinD, LOW);   
+  digitalWrite(pinE, LOW);   
+  digitalWrite(pinF, LOW);   
+  digitalWrite(pinG, LOW);   
+  delay(1000);               ///The waiting time for 1 second
+
+  }
+
+  else if(counter ==2)
+  {
+  digitalWrite(D1, HIGH);
+  digitalWrite(D2, HIGH);
+  digitalWrite(D3, HIGH);
+  digitalWrite(D4, LOW);
+                //for 7 segment to display 7 only pinE, pin F, & pinG is HIGH
+  digitalWrite(pinA, LOW);   
+  digitalWrite(pinB, LOW);   
+  digitalWrite(pinC, LOW);   
+  digitalWrite(pinD, LOW);   
+  digitalWrite(pinE, HIGH);   
+  digitalWrite(pinF, HIGH);   
+  digitalWrite(pinG, HIGH);   
+  delay(1000);               ///The waiting time for 1 second
+
+  }
+
+  else if(counter ==8)
+  {
+  digitalWrite(D1, HIGH);
+  digitalWrite(D2, HIGH);
+  digitalWrite(D3, HIGH);
+  digitalWrite(D4, LOW);
+              //for 7 segment to display 8 ALL pins are LOW
+  digitalWrite(pinA, LOW);   
+  digitalWrite(pinB, LOW);   
+  digitalWrite(pinC, LOW);   
+  digitalWrite(pinD, LOW);   
+  digitalWrite(pinE, LOW);   
+  digitalWrite(pinF, LOW);   
+  digitalWrite(pinG, LOW);   
+  delay(1000);               // The waiting time for 1 second
+
+
+  }
+  else if(counter ==9)
+  {
+  digitalWrite(D1, HIGH);
+  digitalWrite(D2, HIGH);
+  digitalWrite(D3, HIGH);
+  digitalWrite(D4, LOW);
+                //for 7 segment to display 9 only pinE is LOW
+  digitalWrite(pinA, LOW);   
+  digitalWrite(pinB, LOW);   
+  digitalWrite(pinC, LOW);   
+  digitalWrite(pinD, LOW);   
+  digitalWrite(pinE, HIGH);   
+  digitalWrite(pinF, LOW);   
+  digitalWrite(pinG, LOW);   
+  delay(1000);               // The waiting time for 1 second
+
+
+  }
+  delay(500);
+  counter++;
+}
---
+++ 4_7_SegmentDisplay_Transmit.ino
@@ -0,0 +1,124 @@
+//TRANSMITTER BOARD CODE
+#define trigPin  2   //DEFINES TRIGGER PIN
+#define echoPin  3   //DEFINES ECHO PIN
+
+//int LDR = A0;        //INITIALISING LIGHT DEPENDANT RESISTOR PIN
+//unsigned int sensorValue = 0 ;        //INITIALISING A VARIABLE TO STORE LDR VALUE
+
+ int duration;
+ unsigned int distance;                //INITIALISING TWO VARIABLES TO STORE DURATION OF ECHO SIGNAL AND DISTANCE OF OBJECT
+
+
+void setup()            //SETTING UP/DECLARING OF EITHER OUTPUT OR INPUT AND SERIAL MONITORS
+
+{
+ // pinMode(LDR,INPUT);
+  pinMode(trigPin, OUTPUT);           //DECLARE TRIGGERPIN AS OUTPUT PIN
+  pinMode(echoPin, INPUT);            //DECLARE ECHOPIN AS INPUT PIN
+  Serial1.begin(9600);  //BEGENING OF THE ZIGBEE CHANNEL
+}
+
+void loop()             //STARTING ARDUINO LOOP
+{
+     //start front distance computing
+ digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin,HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin,LOW); 
+ float distance = pulseIn(echoPin, HIGH)/58; // return 0 if no high pulse in 1sec
+ Serial.print("Front Distance: ");
+ Serial.print(distance);
+ Serial.println("cm");
+ delay(500);
+
+  if(distance > 0 && distance <10)
+    {
+    Serial1.write(0); //print distance
+    Serial.println(0);
+    }
+    if(distance >= 10 && distance <20)
+    {
+    Serial1.write(1); //print distance
+    Serial.println(1);
+    }
+
+    if(distance >= 20 && distance <30)
+    {
+    Serial1.write(2); //print distance
+    Serial.println(2);
+    }
+    if(distance >= 30 && distance <40)
+    {
+    Serial1.write(3); //print distance
+    Serial.println(3);
+    }
+
+    if(distance >= 40 && distance <50)
+    {
+    Serial1.write(4); //print distance
+    Serial.println(4);
+    }
+    if(distance >= 50 && distance <60)
+    {
+    Serial1.write(5); //print distance
+    Serial.println(5);
+    }
+    if(distance >= 60 && distance <70)
+    {
+    Serial1.write(6); //print distance
+    Serial.println(6);
+    }
+    if(distance >= 70 && distance <80)
+    {
+    Serial1.write(7); //print distance
+    Serial.println(7);
+    }
+    if(distance >= 80 && distance <90)
+    {
+    Serial1.write(8); //print distance
+    Serial.println(8);
+    }
+    if(distance >= 90 && distance <100)
+    {
+    Serial1.write(9); //print distance
+    Serial.println(9);
+    }
+    if(distance >= 100 && distance <110)
+    {
+    Serial1.write(10); //print distance
+    Serial.println(10);
+    }
+    if(distance >= 110 && distance <120)
+    {
+    Serial1.write(11); //print distance
+    Serial.println(11);
+    }
+    if(distance >= 120 && distance <130)
+    {
+    Serial1.write(12); //print distance
+    Serial.println(12);
+    }
+
+    if(distance >= 130 && distance <140)
+    {
+    Serial1.write(13); //print distance
+    Serial.println(13);
+    }
+    if(distance >= 140 && distance <150)
+    {
+    Serial1.write(14); //print distance
+    Serial.println(14);
+    }
+    if(distance >= 150 && distance <160)
+    {
+    Serial1.write(15); //print distance
+    Serial.println(15);
+    }
+    if(distance >= 160)
+    {
+    Serial1.write(16); //print distance
+    Serial.println(16);
+    }
+
+}                        //END OF LOOP
